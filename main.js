c05f91fe12942c21da682c0bde9b93ef95325c66
Elizabeth Q Saico <elizabeth.qsaico@gmail.com>
2016-07-01 09:58:33 -0500

    avance

---
+++ main.js
@@ -0,0 +1,32 @@
+//Escribe aquí tú código
+var listCity = ['Arequipa', 'Tacna','Moquegua','Lima'];
+var listStore = ['Mall Aventura Plaza', 'Parque Lambramani', 'Pepito'];
+var listEmployee = ['Luchito', 'Pepito','Wasaberto'];
+var listSale = [];
+
+function initApp()
+{
+	updateSelect('city',listCity);
+	updateSelect('store',listStore);
+	updateSelect('employee',listEmployee);
+}
+function updateSelectCity(_name,_list)
+{
+	var select = document.getElementById(_name);
+
+	for (var i = 0; i < _list.length; i++) {
+		select.innerHTML += '<option value="' + i + '">' + _list[i] + '</option>';
+	}
+}
+function submitFormStore(event){
+	var sale = $('#sale').val();
+	listSale[parseFloat(sale)];
+	updateSaleEmployee();
+	event.preventDefault();
+	return;
+}
+function updateSaleEmployee(){
+	for(var i= 0; i < listSale.length; i++){
+		listEmployee[i].push(listSale[i]);
+	}
+}
