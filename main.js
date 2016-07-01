43e984adf1341d659d27d194be57e34355d8e9e3
Elizabeth Q Saico <elizabeth.qsaico@gmail.com>
2016-07-01 11:43:03 -0500

    avance

--- main.js
+++ main.js
@@ -3,11 +3,23 @@ var listCity = ['Arequipa', 'Tacna','Moquegua','Lima'];
 var listStore = ['Mall Aventura Plaza', 'Parque Lambramani', 'Pepito'];
 var listEmployee = ['Luchito', 'Pepito','Wasaberto'];
 var listSale = [];
-var _name = "";
-var _list = [];
-
+var listEmployeeSale = [];
+var listStoreEmployee = [];
+var listCityStore = [];
 $(document).ready(initApp);
 
+function employee(_name, _sale){
+	this.name = _name;
+	this.sale = _sale;
+}
+function store(_name,_employee){
+	this.name = _name;
+	this.employee= _employee;
+}
+function city(_name,_store){
+	this.name = _name;
+	this.store= _store;
+}
 function initApp()
 {
 	updateSelect('city',listCity);
@@ -19,18 +31,46 @@ function updateSelect(_name,_list)
 {
 	var select = document.getElementById(_name);
 	for (var i = 0; i < _list.length; i++) {
-		select.innerHTML += '<option value="' + i + '">' + _list[i] + '</option>';
+		select.innerHTML += '<option value="' + _list[i] + '">' + _list[i] + '</option>';
 	}
 }
 function submitFormStore(event){
-	var sale = $('#sale').val();
-	listSale[parseFloat(sale)];
-	updateSaleEmployee();
+	var _city = new city();
+	var _store = new store();
+	var _employee = new employee();
+	_employee.name = $('#employee').val();
+	_employee.sale = $('#sale').val();
+	_store.name = $("#store").val();
+	_store.employee = _employee;
+	_city.name = $("#city").val();
+	_city.store = _store;
+	listEmployeeSale.push(_employee);
+	listStoreEmployee.push(_store);
+	listCityStore.push(_city);
+	var sales = 0;
+	var listReportEmployee = document.getElementById("report_employee");
+	var listReportStore = document.getElementById("report_store");
+	var listReportCity = document.getElementById("report_city");
+	if(listEmployeeSale.length > 0 ){
+		listReportEmployee.innerHTML = ' ';
+	}
+	for(var i = 0; i < listEmployeeSale.length; i++)
+	{
+		sales += parseFloat(listEmployeeSale[i].sale);
+		listReportEmployee.innerHTML += '<li>'+  listEmployeeSale[i].name + ' ' + listEmployeeSale[i].sale + '</li>';
+		if(listStoreEmployee.length > 0 ){
+		listReportStore.innerHTML = ' ';
+		}
+		for(var x=0; x < listStoreEmployee.length; x++){
+			listReportStore.innerHTML += '<li>'+  listStoreEmployee[x].name + ' ' + listStoreEmployee[x].employee.sale + '</li>';
+		if(listCityStore.length > 0 ){
+		listReportCity.innerHTML = ' ';
+		}
+		for(var y=0; x < listCityStore.length; x++){
+			listReportStore.innerHTML += '<li>'+  listStoreEmployee[x].name + ' ' + listStoreEmployee[x].employee.sale + '</li>';
+		}
+		}
+	}
 	event.preventDefault();
 	return;
-}
-function updateSaleEmployee(){
-	for(var i= 0; i < listSale.length; i++){
-		listEmployee[i].push(listSale[i]);
-	}
 }
