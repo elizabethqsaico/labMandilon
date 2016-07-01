e99db6792cc39a64d1af513654929e2c90d6cee3
Elizabeth Q Saico <elizabeth.qsaico@gmail.com>
2016-07-01 10:26:44 -0500

    avance

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ var listCity = ['Arequipa', 'Tacna','Moquegua','Lima'];
 var listStore = ['Mall Aventura Plaza', 'Parque Lambramani', 'Pepito'];
 var listEmployee = ['Luchito', 'Pepito','Wasaberto'];
 var listSale = [];
+var _name = "";
+var _list = [];
+
+$(document).ready(initApp);
 
 function initApp()
 {
 	updateSelect('city',listCity);
 	updateSelect('store',listStore);
 	updateSelect('employee',listEmployee);
+	$('#form_store').submit(submitFormStore);
 }
-function updateSelectCity(_name,_list)
+function updateSelect(_name,_list)
 {
 	var select = document.getElementById(_name);
-
 	for (var i = 0; i < _list.length; i++) {
 		select.innerHTML += '<option value="' + i + '">' + _list[i] + '</option>';
 	}
